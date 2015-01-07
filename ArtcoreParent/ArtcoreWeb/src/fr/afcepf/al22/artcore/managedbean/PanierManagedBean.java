75afe622656b8384dba3b43e03d9249375b425e0
PRJARTCORE <PRJARTCORE@gmail.com>
2015-01-07 13:53:20 +0000

    vider completement le panier, dont le prix total.

--- ArtcoreParent/ArtcoreWeb/src/fr/afcepf/al22/artcore/managedbean/PanierManagedBean.java
+++ ArtcoreParent/ArtcoreWeb/src/fr/afcepf/al22/artcore/managedbean/PanierManagedBean.java
@@ -81,6 +81,7 @@ public class PanierManagedBean {
 	
 	public String supprimerProduit(DtoProduit produit) {
 		this.panier=gestionPanier.retirerProduit(produit);
+		quantite--;
 		loggerPanier();
 		return "";
 	}
@@ -109,8 +110,52 @@ public class PanierManagedBean {
 	 */
 	public void reinitPanier () {
 		log.debug("segtfnhkjldfghgfdsdfghjgfdsqfghdsqdfghj");
+
+		/**
+		 * En fait le panier dans le business n'est pas vide ;
+		 * donc c'est celui-là que je vais chercher à vider.
+		 */
+		for (BlocProduitDto bloc : panier) {
+			gestionPanier.retirerProduit(bloc.getProduit());
+		}
+		
+		/**
+		 * Ya besoin d'initialiser celui ci pour que le produit ne s'affiche plus.
+		 * Et d'initialiser celui du business (plus haut) pour initialiser le prix...
+		 * Et oui !!! :-D
+		 */
 		panier = new ArrayList<BlocProduitDto>();
+//		for (BlocProduitDto bloc : panier) {
+//			log.debug("On supprime l'objet " + bloc + " du panier.");
+//			panier.remove(bloc);
+//		}
+		log.debug("Le panier est maintenant censé être vide. Il contient " + panier.size() + " éléments."); 
 		log.debug("ET MAINTENANT LE PANIER CONTIENT " + panier.size() + " ELEMENTS !!!!!!!!!!!!!!!!!!!!!!");
+		quantite = 0;
+//		int prix = 0;
+		
+		log.debug("LE PRIX TOTAL EST DE " + prixTotal + " EUROS");
+//		prixTotal = null;
+//		prixTotal=BigDecimal.valueOf(prix);
+//		getPrixTotal();
+//		prixTotal=gestionPanier.prixTotal();
+//		prixTotal=new BigDecimal(0);
+		
+		/**
+		 * J'essaye (même si ma liste est censée être vide,
+		 * de supprimer chaque produit du panier.
+		 * Parce que quand on supprime le produit,
+		 * ça met le prix total à jour
+		 * (par magie peut-être, en tout cas je ne sais pas comment).
+		 */
+		for (BlocProduitDto bloc : panier) {
+			gestionPanier.retirerProduit(bloc.getProduit());
+		}
+		/**
+		 * Il ne rentre pas dans mon get...
+		 */
+		log.debug("ET MAINTENANT IL EST CENSE ETRE NULL : " + prixTotal);
+		log.debug("Taille du panier : " + panier.size());
 	}
 	// a effacer
 	public void loggerPanier(){
