704d5a0ec66db08e4910618514c8662563060c9e
PRJARTCORE <PRJARTCORE@gmail.com>
2015-01-08 17:19:21 +0000

    Ajout web service orchestration et message pour stock inssufisant et alert CB ou alert police

--- ArtcoreParent/ArtcoreWeb/src/fr/afcepf/al22/artcore/managedbean/ConfirmerCmdManageBean.java
+++ ArtcoreParent/ArtcoreWeb/src/fr/afcepf/al22/artcore/managedbean/ConfirmerCmdManageBean.java
@@ -1,24 +1,30 @@
 package fr.afcepf.al22.artcore.managedbean;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 import org.apache.log4j.Logger;
 
 import fr.afcepf.al22.artcore.businessinterfaces.IBusinessCommande;
 import fr.afcepf.al22.artcore.businessinterfaces.IBusinessPanier;
+import fr.afcepf.al22.artcore.businessinterfaces.IBusinessRechercherProduit;
 import fr.afcepf.al22.artcore.dto.BlocProduitDto;
 import fr.afcepf.al22.artcore.dto.DtoAdresse;
 import fr.afcepf.al22.artcore.dto.DtoClient;
 import fr.afcepf.al22.artcore.dto.DtoModeDePaiement;
 import fr.afcepf.al22.artcore.dto.DtoProduit;
+import fr.afcepf.al22.orchestration.impl.VerifCommandeImplService;
+import fr.afcepf.al22.orchestration.interfaces.CheckResultProduitCommande;
+import fr.afcepf.al22.orchestration.interfaces.ProduitCommande;
 
 @ManagedBean(name="mbConfirmerCmd")
-@RequestScoped
+@SessionScoped
 public class ConfirmerCmdManageBean {
 	
 	private Logger log= Logger.getLogger(this.getClass());
@@ -30,20 +36,23 @@ public class ConfirmerCmdManageBean {
 	
 	@ManagedProperty(value="#{mbPanier.panier}")
 	private List<BlocProduitDto> panier;
-	// message de confirmation  d'achat.
+	/**Liste des erreur de stock par produit */
+	private List<String> msgErreurStock = new ArrayList<String>();
+	
+	/** message de confirmation  d'achat.*/
 	private String message;
 	
 	private List<DtoModeDePaiement> listMdp;
 	
 	private DtoAdresse adresseChoisie;
 	
-	//mode de paiement selectionné
+	/**mode de paiement selectionné. */
 	private int mdp =1;
 	
-	//adresse de livraison choisie
+	/** adresse de livraison choisie. */
 	private int adrLivraison=1;
 	
-	//Ajout de la carte de paiement
+	/**Ajout de la carte de paiement. */
 	private String numeroCarteDuClient;
 	
 	
@@ -69,7 +78,24 @@ public class ConfirmerCmdManageBean {
 	@EJB
 	private IBusinessCommande daoCommande;
 	
+	@EJB
+	private IBusinessRechercherProduit daoRechercheProduit;
 	
+	/**
+	 * @return the daoRechercheProduit
+	 */
+	public IBusinessRechercherProduit getDaoRechercheProduit() {
+		return daoRechercheProduit;
+	}
+
+	/**
+	 * @param daoRechercheProduit the daoRechercheProduit to set
+	 */
+	public void setDaoRechercheProduit(
+			IBusinessRechercherProduit daoRechercheProduit) {
+		this.daoRechercheProduit = daoRechercheProduit;
+	}
+
 	public String getMessage() {
 		return message;
 	}
@@ -119,30 +145,120 @@ public class ConfirmerCmdManageBean {
 		this.adresseChoisie = adresseChoisie;
 	}
 	
+	/**Convertion d'un blocProduit (Produit.getIdProduit, Quantite) vers ProduitCommande.
+	 * @param paramBlocProduit 
+	 * @return Le ProduitCommande creer.
+	 */
+	private ProduitCommande convertPanierProduitCommande(BlocProduitDto paramBlocProduit) {
+		ProduitCommande retour = new ProduitCommande();
+		log.debug("convert blocProduit "+paramBlocProduit.getProduit().getDescriptionProduit());
+		retour.setIdProduit(paramBlocProduit.getProduit().getIdProduit());
+		log.debug("convert blocProduit quantite "+paramBlocProduit.getQuantite());
+		retour.setQuantiteCommande(paramBlocProduit.getQuantite());
+		return retour;
+	}
+	
+	public String validerCmd() {
+		this.msgErreurStock = null;
+		msgErreurStock = new ArrayList<String>();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		String dateNaissance;
+		message="";
+		log.debug("Message = "+message+" de taille "+message.length());
+		boolean bquantite = true;
+		log.debug("valideCmd avec en panier "+panier.size());
+		log.debug("Init WebService");
+		VerifCommandeImplService verifWS = new VerifCommandeImplService();
 
+		List listeProduit = new ArrayList<ProduitCommande>();
+		log.debug("on convertie le panier");
+		for (BlocProduitDto blocProduitDto :this.mbpanier.getPanier()) {
+			listeProduit.add(convertPanierProduitCommande(blocProduitDto));
+		}
+
+		dateNaissance = sdf.format(dtoClient.getDateNaissance());
+		log.debug("On appel le WebService Orchestration ");
+		log.debug("nom "+dtoClient.getNomClient()+" prenom "+dtoClient.getPrenomClient());
+		log.debug(" date "+dateNaissance+" numCarte "+numeroCarteDuClient);
+		
+		CheckResultProduitCommande validation = verifWS.getVerifCommandeImplPort().verifCommande(dtoClient.getNomClient(),
+												dtoClient.getPrenomClient(),dateNaissance ,
+												numeroCarteDuClient, listeProduit);
+
+		log.debug("Retour Appel WS Validation commande *******************");
+		log.debug("Validation alertePolice "+validation.getAlertePolice()+"!");
+		log.debug("validation AlertCB "+validation.isAlerteCB());
+		log.debug("validation listeProduits "+validation.getListeProduits());
+		log.debug("***************************************");
+		
+		log.debug("verif alerte police");
+		if (!"".equalsIgnoreCase(validation.getAlertePolice())) {
+			this.message = "Vous n'ête pas authorisé car vous fiche par la police pour "+validation.getAlertePolice();
+			log.debug("police msg "+this.message);
+		}
+		if (validation.isAlerteCB()) {
+			message=" Carte de paiement invalide !!!";
+			log.debug("isAlerteCB msg "+message);
+		}
+		if (!message.isEmpty()) {
+			log.debug("message.isEmpty "+message);
+			return "/bravo.jsf?faces-redirect=true";
+		}
+		
+		//Dans tous les autres cas
+		log.debug("*************************List Produits*******************");
+		log.debug("list produits size "+validation.getListeProduits().size());
+		for (ProduitCommande p : validation.getListeProduits()) {
+			DtoProduit dto;
+			log.debug("p.getQuantiteCommande = "+p.getQuantiteCommande());
+			if (p.getQuantiteCommande()<0) {
+				log.debug("Produit manque stock "+p.getIdProduit()+" "+p.getQuantiteCommande());
+				message="Produit manquant "+p.getIdProduit()+" "+p.getQuantiteCommande();
+				dto = daoRechercheProduit.produitParId(p.getIdProduit());
+				this.msgErreurStock.add("Il manque "+Math.abs(p.getQuantiteCommande())+" pour le produit "+dto.getLibelleProduit()+".");
+				log.debug("message added : "+"Il manque "+Math.abs(p.getQuantiteCommande())+" pour le produit "+dto.getLibelleProduit()+".");
+				bquantite = false;
+			}	
+		}
+		if (!bquantite) {
+			log.debug("message "+message);
+			return "/afficherPanier.jsf?faces-redirect=true";
+		}
+		return "";
+	}
+	
+	/**Valide la commande via WebService l'appel methode {{@link #validerCmd()} puis enregistre la commande en base via DAO
+	 * @return
+	 */
 	public String confirmerCmd(){
+		boolean result=true;
+		
+		String redirection = validerCmd();
+		if (redirection!= "") {
+			return redirection;
+		}
+		
+		
 		log.debug("confirmation du panier");
-		boolean result=false;
+
 		if (mdp > 0) {
-			
-		result=daoCommande.validerCmd(panier, mdp, dtoClient,daoCommande.rechercherAdresse(adrLivraison));
-		log.debug("retour du dao result = "+result);
-		mbpanier.reinitPanier();
+			result=daoCommande.validerCmd(panier, mdp, dtoClient,daoCommande.rechercherAdresse(adrLivraison));
+			log.debug("retour du dao result = "+result);
+			if (result) {
+				mbpanier.reinitPanier();
+				message="Felicitation ! votre commande a été validée ";
+				log.debug("message ="+message);
+				redirection = "/bravo.jsf?faces-redirect=true";
+			} else {
+				message="Une erreure est survenue pendant la création de votre commande ! ";
+				log.debug("message "+message);
+				redirection = "/afficherPanier.jsf?faces-redirect=true";
+			}
 		}
-		else {
-			//FIXME Etrange else avec un  syso ? :(
-			log.debug("booooooooooooooooooooooooooooooooouh");
-		}
-		
-		if(result){
-			
-			message="Felicitation ! votre commande a été validée ";
-			log.debug("message ="+message);
-			
-			
-		}
-		
-		return "/bravo.jsf?faces-redirect=true";
+
+		log.debug("message "+message);
+		return redirection; 
 	}
 	
 	public String bravo() {
@@ -172,6 +288,20 @@ public class ConfirmerCmdManageBean {
 
 	public void setMbpanier(PanierManagedBean mbpanier) {
 		this.mbpanier = mbpanier;
+	}
+
+	/**
+	 * @return the msgErreurStock
+	 */
+	public List<String> getMsgErreurStock() {
+		return msgErreurStock;
+	}
+
+	/**
+	 * @param msgErreurStock the msgErreurStock to set
+	 */
+	public void setMsgErreurStock(List<String> msgErreurStock) {
+		this.msgErreurStock = msgErreurStock;
 	}
 	
 
