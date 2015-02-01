f37797c6f96159957e01e52507bf6bbee1af03de
PRJARTCORE <PRJARTCORE@gmail.com>
2015-02-01 14:14:10 +0000

    ajout des nouvelles fonctionnalitées.

--- Geocodeur/src/main/java/pack/test/FindMongo.java
+++ Geocodeur/src/main/java/pack/test/FindMongo.java
@@ -18,9 +18,16 @@ public class FindMongo {
 			e.printStackTrace();
 		}
 		Morphia morphia = new Morphia();
-		dao = new GalerieDao(mongo,morphia,"galerieDB");
+		dao = new GalerieDao(mongo,morphia,"galerie3db");
+		System.out.println("*****recherche par within 2.33,48.859,0.8/6371 *****");
+		lines = dao.findGPS(2.33,48.859,0.8/6371);
 		
-		lines = dao.findGPS(2.33,48.859,0.5/6371);
+		System.out.println("lines size "+lines.size());
+		for(GalerieEntity ge : lines) {
+			System.out.println("line : "+ge.getId()+" "+ge.getLoc()[0]+" "+ge.getLoc()[1]+" "+ge.getNomGalerie()+" "+ge.getFormattedAdresse());
+		}
+		System.out.println("*****recherche par near *****");
+		lines = dao.nearGps(2.33,48.859,0.8/6371);
 		
 		System.out.println("lines size "+lines.size());
 		for(GalerieEntity ge : lines) {
