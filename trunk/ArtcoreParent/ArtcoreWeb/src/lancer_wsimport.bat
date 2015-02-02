00ad2f90152ea4a76b20fd6b16e69fbe096e5d2f
PRJARTCORE@gmail.com <PRJARTCORE@gmail.com@d99d448d-b036-b2e4-ad59-d56b6dee7154>
2015-02-02 10:04:02 +0000

    ajout de la possibilité de parametrer l'adresse ip du webservice.

---
+++ trunk/ArtcoreParent/ArtcoreWeb/src/lancer_wsimport.bat
@@ -0,0 +1,8 @@
+﻿
+cd /d "%~dp0"
+
+REM "C:\Program Files\Java\jdk1.7.0_67\bin\wsimport" -keep -s ws\src  -d ws http://192.168.100.129:9090/WSOrchestrator/VerifCommandeImpl?wsdl
+"C:\Program Files\Java\jdk1.7.0_67\bin\wsimport" -Xnocompile -s .  main\resources\wsdlOrchestrator.wsdl
+
+pause
+REM LE WS doit être accessible.
