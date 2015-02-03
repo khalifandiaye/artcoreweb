19e0dfe7b93bb7949be37508f43a31af4f533fc2
PRJARTCORE@gmail.com <PRJARTCORE@gmail.com@d99d448d-b036-b2e4-ad59-d56b6dee7154>
2015-02-03 13:16:26 +0000

    stabilisation

--- trunk/ArtcoreParent/ArtcoreWeb/src/lancer_wsimport.bat
+++ trunk/ArtcoreParent/ArtcoreWeb/src/lancer_wsimport.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 
 REM "C:\Program Files\Java\jdk1.7.0_67\bin\wsimport" -keep -s ws\src  -d ws http://192.168.100.129:9090/WSOrchestrator/VerifCommandeImpl?wsdl
-"C:\Program Files\Java\jdk1.7.0_67\bin\wsimport" -Xnocompile -s .  main\resources\wsdlOrchestrator.wsdl
+"C:\Program Files\Java\jdk1.7.0_67\bin\wsimport" -Xno-addressing-databinding -Xnocompile -s .  main\resources\wsdlOrchestrator.wsdl
 
 pause
 REM LE WS doit être accessible.
